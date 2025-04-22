0e2a08442e53e22f68b1afc8028d57371c8f4afc
chenhaipei01 <15129544+chenhaipei01@user.noreply.gitee.com>
2025-04-23 01:42:56 +0800

    nih

--- setup.sh
+++ setup.sh
@@ -4,9 +4,14 @@
 docker compose pull
 docker pull hummingbot/hummingbot:latest
 
-# Creating .env file with the required environment variables
-echo "CONFIG_PASSWORD=a" > .env
-echo "BOTS_PATH=$(pwd)" >> .env
+# Adding required environment variables if they don't exist
+if [ ! -f .env ]; then
+    echo "CONFIG_PASSWORD=a" > .env
+    echo "BOTS_PATH=$(pwd)" >> .env
+else
+    grep -q "CONFIG_PASSWORD" .env || echo "CONFIG_PASSWORD=a" >> .env
+    grep -q "BOTS_PATH" .env || echo "BOTS_PATH=$(pwd)" >> .env
+fi
 
 # Running docker-compose in detached mode
 docker compose up -d
